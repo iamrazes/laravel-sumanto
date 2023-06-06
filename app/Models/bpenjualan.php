5486fd037ec32740ae957f5e1be5a8d7d11a92a6
RAZES <iamrazes.official@gmail.com>
2023-06-06 09:36:07 +0700

    minor fixes

--- app/Models/bpenjualan.php
+++ app/Models/bpenjualan.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class bpenjualan extends Model
+class Bpenjualan extends Model
 {
     use HasFactory;
 
